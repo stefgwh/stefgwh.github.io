c248113b69f380c54c11102d16f7e49a13e32260
stefgwh <47600753+stefgwh@users.noreply.github.com>
2023-01-21 19:45:47 +0100

    Update line_chart_plotly.js

--- SAC/CustomWidget/LineChartPlotly/line_chart_plotly.js
+++ SAC/CustomWidget/LineChartPlotly/line_chart_plotly.js
@@ -1,88 +1,99 @@
 var getScriptPromisify = (src) => {
-  return new Promise((resolve) => {
-    $.getScript(src, resolve);
-  });
+    return new Promise((resolve) => {
+        $.getScript(src, resolve);
+    });
 };
 
 (function () {
-  let prepared = document.createElement("template");
-  prepared.innerHTML = `
-          <style>
-          </style>
-          <div id="root" style="width: 100%; height: 100%;">
-          </div>
-        `;
+    let prepared = document.createElement("template");
+    prepared.innerHTML = `
+            <style>
+            </style>
+            <div id="root" style="width: 100%; height: 100%;">
+            </div>
+          `;
 
-  class LinePlotlyDemo extends HTMLElement {
+    class LinePlotlyDemo extends HTMLElement {
 
+        constructor() {
+            super();
 
-    constructor() {
-      super();
-      
-      this._shadowRoot = this.attachShadow({ mode: "open" });
-      this._shadowRoot.appendChild(prepared.content.cloneNode(true));
+            this._shadowRoot = this.attachShadow({ mode: "open" });
+            this._shadowRoot.appendChild(prepared.content.cloneNode(true));
 
-      this._root = this._shadowRoot.getElementById("root");
-      
-      this.addEventListener("click", event => {
-				var event = new Event("onClick");
-				this.dispatchEvent(event);
-			});
-    }
+            this._root = this._shadowRoot.getElementById("root");
 
+            this.render();
 
-    //Fired when the widget is added to the html DOM of the page
-    connectedCallback() {
-      this._firstConnection = true;
-      this.redraw();
-    }
-
-    //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-    disconnectedCallback() {
-
-    }
-
-    //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-    onCustomWidgetBeforeUpdate(oChangedProperties) {
-      this.redraw();
-    }
-
-    //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-    onCustomWidgetAfterUpdate(oChangedProperties) {
-      if (this._firstConnection) {
-        this.redraw();
-      }
-    }
-
-    //When the custom widget is removed from the canvas or the analytic application is closed
-    onCustomWidgetDestroy() {
-    }
-
-    //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-    // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-    //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-    /*
-    onCustomWidgetResize(width, height){
-    
-    }
-    */
-
-    async redraw() {
-      await getScriptPromisify(
-        "https://cdn.plot.ly/plotly-2.17.1.min.js");
-
-      var data = [
-        {
-          x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-          y: [1, 3, 6],
-          type: 'scatter'
         }
-      ];
 
-      Plotly.newPlot(this._root, data);
 
+        set myDataSource(dataBinding) {
+            this._myDataSource = dataBinding;
+            this.render();
+        }
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback() {
+            this._firstConnection = true;
+            this.render();
+        }
+
+        //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback() {
+
+        }
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+        onCustomWidgetBeforeUpdate(oChangedProperties) {
+            this.render();
+        }
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+        onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection) {
+                this.render();
+            }
+        }
+
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy() {
+        }
+
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+
+        onCustomWidgetResize(width, height) {
+            this.render();
+        }
+
+
+        async render() {
+            await getScriptPromisify(
+                "https://cdn.plot.ly/plotly-2.17.1.min.js"
+            );
+
+            /*
+            if (!this._myDataSource || this._myDataSource.state !== "success") {
+                return;
+            }
+            */
+
+            var data = [
+                {
+                    x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+                    y: [1, 3, 6],
+                    type: 'scatter'
+                }
+            ];
+
+            Plotly.newPlot(this._root, data, {
+                title: "Test"
+              });
+
+        }
     }
-  }
 
-  customElements.define("com-sap-sample-template", LinePlotlyDemo);
+    customElements.define("com-sap-sample-template", LinePlotlyDemo);
 })();
